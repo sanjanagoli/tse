40cea5a74bc5217eef36eab94aa35294a601ce09
Sanjana Goli <sanjana.goli@gmail.com>
2019-05-06 18:46:26 -0400

    testing program

---
+++ indexer/testing.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# testing.sh - testing for indexer.c and indextest.c
+# Sanjana Goli, April 2019
+# 
+#
+# Description: Test cases for indexer.c and indextest.c
+
+# INDEXER TESTING
+
+# More than three arguments
+./indexer testDir oldIndexFile.txt extraarg
+
+# Not a crawler directory
+./indexer ../common oldIndexFile.txt
+
+# Not writable file
+./indexer testDir notwritable.txt
+
+# Working test
+./indexer testDir oldIndexFile.txt
+
+# Working test2
+./indexer testDir2 indexFile1.txt
+
+# INDEXTEST TESTING
+
+# first input file does not exist
+./indextest water.txt new.txt
+
+# second file (file that contains output) is not writable
+./indextest oldIndexFile.txt notwritable.txt
+
+# actual case --> using input from correct indexer case above
+./indextest oldIndexFile.txt newIndexFile.txt
+
+# actual case2 --> using input from correct indexer case above
+./indextest indexFile1.txt indexFile2.txt
+
+# Confirm success of program
+
+gawk -f indexsort.awk indexFile1.txt > indexFile1sorted.txt
+gawk -f indexsort.awk indexFile2.txt > indexFile2sorted.txt
+
+diff indexFile2sorted.txt indexFile1sorted.txt
+if [ $? -eq 0 ]; then 
+	echo success
+fi
