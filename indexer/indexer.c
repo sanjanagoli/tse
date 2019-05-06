ae4d002ae03d11eea7ca9a4d15b711cf041cb523
Sanjana Goli <sanjana.goli@gmail.com>
2019-05-05 20:49:16 -0400

    small changes

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -20,20 +20,23 @@ main(int argc, char *argv[])
 
 	//create variables for inputs to manipulate later
 	if(argc == 3) {
-		char *directory = (char *)malloc((strlen(argv[1]))*sizeof(char));
+		char *directory = (char *)malloc((strlen(argv[1])+1)*sizeof(char));
 		strcpy(directory, argv[1]);
-		char *indexFilename = (char *)malloc((strlen(argv[2]))*sizeof(char));
+		char *indexFilename = (char *)malloc((strlen(argv[2])+1)*sizeof(char));
 		strcpy(indexFilename, argv[2]);
 
 		//checks whether the directory is produced by crawler
 		if (isCrawlerDirectory(directory)) {
-			index_t *index = index_new();
+			index_t *index = index_new(250);
 			index_build(directory, index);
 			index_save(indexFilename, index);
-			printf("ayyaya\n");
+
+			index_delete(index);
 		}
 
-
+		free(directory);
+		free(indexFilename);
+		
 	} else {
 		fprintf(stderr, "usage: ./indexer pageDirectory indexFilename");
 		return 1;
--- indexer/indextest.c
+++ indexer/indextest.c
@@ -23,21 +23,24 @@ main(int argc, char *argv[])
 		char *newIndexFilename = (char *)malloc((strlen(argv[2])+1)*sizeof(char));
 		strcpy(newIndexFilename, argv[2]);
 
-		int sizeOfTable = (int) (get_num_lines(oldIndexFilename)/10);
+		int lines;
+		int sizeOfTable;
+		if ((lines = get_num_lines(oldIndexFilename)) != -1) {
+			sizeOfTable = (int) (lines/10);
+
+		} else {
+			fprintf(stderr, "There were no lines in the %s file", oldIndexFilename);
+			return 1;
+		}
 		
 		index_t *index2 = index_new(sizeOfTable);
 
-		//******need to check if the file exists
 		index_load(oldIndexFilename, index2);
-		
 		index_save(newIndexFilename, index2);
 
 		index_delete(index2);
-
 		free(oldIndexFilename);
 		free(newIndexFilename);
-		printf("%d", sizeOfTable);
-		printf("reached indextest");
 
 	} else {
 		fprintf(stderr, "usage: ./indextest oldIndexFilename newIndexFilename");
